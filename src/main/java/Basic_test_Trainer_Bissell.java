a078dff1c6dc69e3cf09c948120bd1d8a6a9ae36
andrasragany <randris@gmail.com>
2020-01-25 08:47:20 +0100

    elso

--- src/main/java/Basic_test_Trainer_Bissell.java
+++ src/main/java/Basic_test_Trainer_Bissell.java
@@ -1,4 +1,6 @@
 import org.openqa.selenium.StaleElementReferenceException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -27,55 +29,50 @@ public class Basic_test_Trainer_Bissell{
         SimpleFormatter formatter = new SimpleFormatter();
         fh.setFormatter(formatter);
 
-        //WebDriver webDriver = new ChromeDriver();
-        //WebDriverWait wait = (WebDriverWait) new WebDriverWait(webDriver, 5).ignoring(StaleElementReferenceException.class);
+        WebDriver webDriver = new ChromeDriver();
+        WebDriverWait wait = (WebDriverWait) new WebDriverWait(webDriver, 5).ignoring(StaleElementReferenceException.class);
 
         FirefoxDriver ffDriver = new FirefoxDriver();
         WebDriverWait wait_ff = (WebDriverWait) new WebDriverWait(ffDriver, 5).ignoring(StaleElementReferenceException.class);
 
-        //String UserJsonPath = "c:\\Users\\Rendszergazda\\IdeaProjects\\platformtest\\src\\main\\java\\user.json";
-        //String UserJsonPath = "c:\\Users\\randr\\IdeaProjects\\platformtest\\src\\main\\java\\user.json";
-        //Object obj = new JSONParser().parse(new FileReader(UserJsonPath));
-        //JSONObject jo = (JSONObject) obj;
-
-        //webDriver.manage().window().maximize();
+        webDriver.manage().window().maximize();
         ffDriver.manage().window().maximize();
 
         //Open page
-        //_fc.gotourl(logger,webDriver,"https://test.bissellexpert.com/login");
+        _fc.gotourl(logger,webDriver,"https://test.bissellexpert.com/login");
         _fc.gotourl(logger,ffDriver,"https://test.bissellexpert.com/login");
         Thread.sleep(1000);
 
         //Login
-        //_fc.login(logger, webDriver, wait, "bisselltrainer", "bisselltrainerpassword");
+        _fc.login(logger, webDriver, wait, "bisselltrainer", "bisselltrainerpassword");
         _fc.login(logger, ffDriver, wait_ff, "bisselltrainer", "bisselltrainerpassword");
 
         Thread.sleep(2000);
 
         //create trainer group for quince trainer for comm
-        //_fc.navigatetousergroups(logger, webDriver, wait, "trainer");
+        _fc.navigatetousergroups(logger, webDriver, wait, "trainer");
         _fc.navigatetousergroups(logger, ffDriver, wait_ff, "trainer");
-        //String userGroupNameChrome1 = _fc.create_usergroup(logger, webDriver, wait, "Quince Trainer");
-        //String userGroupNameChrome_2 = _fc.create_usergroup(logger, webDriver, wait, "Quince Student");
+        String userGroupNameChrome1 = _fc.create_usergroup(logger, webDriver, wait, "Quince Trainer");
+        String userGroupNameChrome_2 = _fc.create_usergroup(logger, webDriver, wait, "Quince Student");
         String userGroupNameFF1 = _fc.create_usergroup(logger, ffDriver, wait_ff, "Trainer");
         String userGroupNameFF_2 = _fc.create_usergroup(logger, ffDriver, wait_ff, "Student");
 
-        //create communication
-        //_fc.navigatetocommunication(logger, webDriver, wait, "trainer");
+        _fc.navigatetocommunication(logger, webDriver, wait, "trainer");
         _fc.navigatetocommunication(logger, ffDriver, wait_ff, "trainer");
 
-        //_fc.createcommunication(logger, webDriver, wait, userGroupNameChrome1);
+        _fc.createcommunication(logger, webDriver, wait, userGroupNameChrome1);
         _fc.createcommunication(logger, ffDriver, wait_ff, userGroupNameFF1);
 
-        //_fc.create_training(webDriver, wait, logger, userGroupNameChrome_2);
+        _fc.create_training(webDriver, wait, logger, userGroupNameChrome_2);
         _fc.create_training(ffDriver, wait_ff, logger, userGroupNameFF_2);
 
+        _fc.create_LP(webDriver, wait, logger,userGroupNameChrome_2);
         _fc.create_LP(ffDriver, wait_ff, logger,userGroupNameFF_2);
 
 
 
         logger.info("Trainer Test finished OK");
-        //webDriver.quit();
+        webDriver.quit();
         ffDriver.quit();
     }
 }
--- src/main/java/Basic_test_Trainer_Philips.java
+++ src/main/java/Basic_test_Trainer_Philips.java
@@ -1,6 +1,7 @@
 import org.openqa.selenium.StaleElementReferenceException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.io.File;
@@ -28,43 +29,42 @@ public class Basic_test_Trainer_Philips{
         WebDriver webDriver = new ChromeDriver();
         WebDriverWait wait = (WebDriverWait) new WebDriverWait(webDriver, 5).ignoring(StaleElementReferenceException.class);
 
-        //FirefoxDriver ffDriver = new FirefoxDriver();
-        //WebDriverWait wait_ff = (WebDriverWait) new WebDriverWait(ffDriver, 5).ignoring(StaleElementReferenceException.class);
+        FirefoxDriver ffDriver = new FirefoxDriver();
+        WebDriverWait wait_ff = (WebDriverWait) new WebDriverWait(ffDriver, 5).ignoring(StaleElementReferenceException.class);
 
         webDriver.manage().window().maximize();
-        //ffDriver.manage().window().maximize();
+        ffDriver.manage().window().maximize();
 
         //Open page
         _fc.gotourl(logger,webDriver,"https://test.philipsohcacademy.com//login");
-        //_fc.gotourl(logger,ffDriver,"https://test.philipsohcacademy.com/login");
+        _fc.gotourl(logger,ffDriver,"https://test.philipsohcacademy.com/login");
         //Login
         _fc.login(logger, webDriver, wait, "philipstrainer", "philipstrainerpassword");
-        //_fc.login(logger, ffDriver, wait_ff, "philipstrainer", "philipstrainerpassword");
+        _fc.login(logger, ffDriver, wait_ff, "philipstrainer", "philipstrainerpassword");
         Thread.sleep(1000);
 
         //create trainer group for quince trainer for comm
         _fc.navigatetousergroups(logger, webDriver, wait, "trainer");
-        //_fc.navigatetousergroups(logger, ffDriver, wait_ff, "trainer");
+        _fc.navigatetousergroups(logger, ffDriver, wait_ff, "trainer");
         String userGroupNameChrome1 = _fc.create_usergroup(logger, webDriver, wait, "Trainer");
         String userGroupNameChrome_2 = _fc.create_usergroup(logger, webDriver, wait, "Student");
-        //String userGroupNameFF1 = _fc.create_usergroup(logger, ffDriver, wait_ff, "Trainer");
-        //String userGroupNameFF_2 = _fc.create_usergroup(logger, ffDriver, wait_ff, "Student");
+        String userGroupNameFF1 = _fc.create_usergroup(logger, ffDriver, wait_ff, "Trainer");
+        String userGroupNameFF_2 = _fc.create_usergroup(logger, ffDriver, wait_ff, "Student");
 
-        //create communication
         _fc.navigatetocommunication(logger, webDriver, wait, "trainer");
-        //_fc.navigatetocommunication(logger, ffDriver, wait_ff, "trainer");
+        _fc.navigatetocommunication(logger, ffDriver, wait_ff, "trainer");
 
         _fc.createcommunication(logger, webDriver, wait, userGroupNameChrome1);
-        //_fc.createcommunication(logger, ffDriver, wait_ff, userGroupNameFF1);
+        _fc.createcommunication(logger, ffDriver, wait_ff, userGroupNameFF1);
 
         _fc.create_training(webDriver, wait, logger, userGroupNameChrome_2);
-        //_fc.create_training(ffDriver, wait_ff, logger, userGroupNameFF_2);
+        _fc.create_training(ffDriver, wait_ff, logger, userGroupNameFF_2);
 
         _fc.create_LP(webDriver, wait, logger,userGroupNameChrome_2);
-        //_fc.create_LP(ffDriver, wait_ff, logger,userGroupNameFF_2);
+        _fc.create_LP(ffDriver, wait_ff, logger,userGroupNameFF_2);
 
         logger.info("Trainer Test finished OK");
         webDriver.quit();
-        //ffDriver.quit();
+        ffDriver.quit();
     }
 }
